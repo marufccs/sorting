e1d8355404f335266cfafc441643f2b70db6a759
unknown <marufmozumder7@gmail.com>
2023-04-09 02:14:52 +0200

    linked-list-done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollections } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([10, 3, -3, 9]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection);
-const charactersCollection = new CharactersCollections("sakdfsl");
-const sorter = new Sorter(charactersCollection);
+
+// const charactersCollection = new CharactersCollections("sakdfsl");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(300);
+linkedList.add(500);
+linkedList.add(-50);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection);
+linkedList.print();
