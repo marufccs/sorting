ad72da0e969eeb9a09bd3da5d66153e7407965de
unknown <marufmozumder7@gmail.com>
2023-04-09 02:06:57 +0200

    characters-collection-done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollections } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -3, 9]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -3, 9]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection);
+const charactersCollection = new CharactersCollections("sakdfsl");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection);
+console.log(charactersCollection);
